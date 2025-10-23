bcc745bf94fe017cc12f2ee47a65b953251fa141
Volodymyr Slobodian <opitral@proton.me>
2025-10-23 18:13:00 +0300

    feat(#63): [GREEN] Implement export method to pass empty list test (resolves #63)

--- expensetracker/src/main/java/com/example/expensetracker/exporters/CsvExporter.java
+++ expensetracker/src/main/java/com/example/expensetracker/exporters/CsvExporter.java
@@ -1,4 +1,18 @@
 package com.example.expensetracker.exporters;
 
+import com.example.expensetracker.entity.ExpenseEntity;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
 public class CsvExporter {
+
+    public void exportExpenses(Writer writer, List<ExpenseEntity> expenses) {
+        try {
+            writer.write("ID,Date,Category,Amount,Description\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
