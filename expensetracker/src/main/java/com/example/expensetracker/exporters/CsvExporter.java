52adef15729de78577cf9909b4227e5a436f56c8
Volodymyr Slobodian <opitral@proton.me>
2025-10-23 18:23:19 +0300

    feat(#64): [REFACTOR] Extract line formatting to private helper method (resolves #64)

--- expensetracker/src/main/java/com/example/expensetracker/exporters/CsvExporter.java
+++ expensetracker/src/main/java/com/example/expensetracker/exporters/CsvExporter.java
@@ -13,19 +13,21 @@ public class CsvExporter {
     public void exportExpenses(Writer writer, List<ExpenseEntity> expenses) {
         try {
             writer.write(CSV_EXPENSES_HEADER);
-
             for (ExpenseEntity expense : expenses) {
-                String line = String.format("%d,%s,%s,%s,%s\n",
-                        expense.getId(),
-                        expense.getDate().toString(),
-                        expense.getCategory().getName(),
-                        expense.getAmount().toPlainString(),
-                        expense.getDescription()
-                );
-                writer.write(line);
+                writer.write(formatExpenseAsCsvLine(expense));
             }
         } catch (IOException e) {
             throw new RuntimeException("Error writing CSV data", e);
         }
+    }
+
+    private String formatExpenseAsCsvLine(ExpenseEntity expense) {
+        return String.format("%d,%s,%s,%s,%s\n",
+                expense.getId(),
+                expense.getDate().toString(),
+                expense.getCategory().getName(),
+                expense.getAmount().toPlainString(),
+                expense.getDescription()
+        );
     }
 }
