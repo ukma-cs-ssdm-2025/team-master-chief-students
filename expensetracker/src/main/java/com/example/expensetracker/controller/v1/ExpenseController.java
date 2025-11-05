1d4135e7301a980e593e790061eb3fd8e490cde0
Volodymyr Slobodian <opitral@proton.me>
2025-11-05 17:38:05 +0200

    add pdf exporter, service method and endpoint

--- expensetracker/src/main/java/com/example/expensetracker/controller/v1/ExpenseController.java
+++ expensetracker/src/main/java/com/example/expensetracker/controller/v1/ExpenseController.java
@@ -28,6 +28,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.OutputStream;
 import java.io.Writer;
 
 @RestController
@@ -394,6 +395,18 @@ public class ExpenseController {
             exportService.exportUserExpensesToCsv(writer);
         } catch (Exception e) {
             throw new AppException("Error exporting expenses to CSV", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/export/pdf")
+    public void exportToPdf(HttpServletResponse response) {
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=\"expenses.pdf\"");
+
+        try (OutputStream outputStream = response.getOutputStream()) {
+            exportService.exportUserExpensesToPdf(outputStream);
+        } catch (Exception e) {
+            throw new AppException("Error exporting expenses to PDF", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
