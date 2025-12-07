a773741e0ae825143d5a32459607cf745a70a157
Ermol <00ermzahar@gmail.com>
2025-12-08 00:09:16 +0300

    feat: update JWT filter to exclude authentication for auth endpoints and add CORS support for www subdomain

--- expensetracker/src/main/java/com/example/expensetracker/config/SecurityConfig.java
+++ expensetracker/src/main/java/com/example/expensetracker/config/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig {
                                   "http://localhost:5173",
                                   "http://195.189.226.101",
                                   "https://195.189.226.101",
-                                  "https://expense-tracker.pp.ua"
+                                  "https://expense-tracker.pp.ua",
+                                  "https://www.expense-tracker.pp.ua"
                               ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
